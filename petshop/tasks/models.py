41831c37c9747c9a1a329cd0004655048d85831a
Felipe Carneiro <felipe_carneiro@outlook.com.br>
2021-11-15 17:30:17 -0300

    editando novos campos ao banco de dados

--- petshop/tasks/models.py
+++ petshop/tasks/models.py
@@ -5,15 +5,24 @@ STATUS = (
     ('done', 'Done'),
 )
 
+TYPE = (
+    ('gato' , 'Gato'),
+    ('cachorro' , 'Cachorro'),
+    ('passaro' , 'Passaro'),
+)
+
+
 class Task(models.Model):
     name = models.CharField(max_length=100, verbose_name="Nome")
     phone = models.CharField(max_length=11, verbose_name='Telefone', default=False)
     email = models.EmailField(blank=True, null=True,
                               verbose_name='E-mail', default=False)
-    login = models.CharField(
-        max_length=100, verbose_name='Login', default=False)
-    password = models.CharField(
-        max_length=100, verbose_name='Senha', default=False)
+    animal_race = models.CharField(
+    max_length=50,
+    SPECIES=TYPE,
+    verbose_name='Tipo de animal',
+    )
+
     done = models.CharField(
         max_length=15,
         choices=STATUS,
