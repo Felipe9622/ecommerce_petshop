b36452cd957ed9ef9069217ffef1dffbfa8492f4
Felipe Carneiro <felipe_carneiro@outlook.com.br>
2021-11-15 01:51:16 -0300

    criando template para usuarios cadastrados

--- petshop/tasks/urls.py
+++ petshop/tasks/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('', views.Pagina),
     path('cadastro/', views.Cadastro, name="new-data"),
     path('login/', views.Login, name="new-data"),
-    path('usuario/', views.Usuario, name="new-data"),
+    path('usuario/', views.Usuario),
     path('dados/', ListAndCreate.as_view()),
     path('dados/<int:pk>/', DetailAndDelete.as_view()),
 ]
