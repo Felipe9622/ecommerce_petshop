e4284da52a2d29eb940e6963229be7c28b69f144
Felipe Carneiro <felipe_carneiro@outlook.com.br>
2021-11-25 23:07:11 -0300

    arrumando o ForeignKey da class ProductAttribute

--- petshop/ecommerce/admin.py
+++ petshop/ecommerce/admin.py
@@ -6,14 +6,13 @@ admin.site.register(Category)
 admin.site.register(Brand)
 admin.site.register(Size)
 
+#Modelo do Produto
 class ProductAdmin(admin.ModelAdmin):
     list_display=('id', 'title', 'brand', 'size', 'status')
-    list_editable=('status')
+    list_editable=('status',)#precisa manter a , logo apos que colocar 'status' se tirar vai apresentar uma mensagem de erro
 admin.site.register(Product,ProductAdmin)
 
 # Atributos do Produto
-
-
 class ProductAttributeAdmin(admin.ModelAdmin):
-    list_display = ('id', 'product', 'price', 'size' )
+    list_display=('id', 'product', 'price', 'size' )
 admin.site.register(ProductAttribute, ProductAttributeAdmin)
--- petshop/ecommerce/models.py
+++ petshop/ecommerce/models.py
@@ -58,10 +58,11 @@ class Product(models.Model):
     def __str__(self):
         return self.title
 
+
 # Atributos do Produto
 class ProductAttribute(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    size = models.ForeignKey(Product, on_delete=models.CASCADE)
+    size = models.ForeignKey(Size, on_delete=models.CASCADE)
     price = models.PositiveIntegerField()
 
     def __str__(self):
