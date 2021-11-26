77210c9fe31077d65d380999c27b1c75b2bf5049
Felipe Carneiro <felipe_carneiro@outlook.com.br>
2021-11-25 22:11:34 -0300

    cs

--- petshop/ecommerce/models.py
+++ petshop/ecommerce/models.py
@@ -5,7 +5,7 @@ class Category(models.Model):
     title = models.CharField(max_length=100,
     verbose_name='categoria do produto')
 
-    image = models.ImageField(upload_to='categoria',
+    image = models.ImageField(upload_to='dadosCategory',
     verbose_name='imagem do produto')
 
     def __str__(self):
@@ -16,7 +16,7 @@ class Brand(models.Model):
     title = models.CharField(max_length=100,
     verbose_name='marca do produto')
 
-    image = models.ImageField(upload_to='marca',
+    image = models.ImageField(upload_to='dadosBrand',
     verbose_name='imagem do produto')
 
     def __str__(self):
@@ -36,7 +36,8 @@ class Size(models.Model):
 #Modelo do Produto
 class Product(models.Model):
     title = models.CharField(max_length=200)
-    image = models.ImageField(upload_to='produto')
+    image = models.ImageField(upload_to='produto')#upload_to ciria uma pasta e todas as fotos que 
+    #forem adicionadas ficaram guardadas na pasta 
     slug = models.CharField(max_length=400)
     detail = models.TextField()
     specs = models.TextField()
@@ -46,6 +47,7 @@ class Product(models.Model):
     brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
     size = models.ForeignKey(Size, on_delete=models.CASCADE)
     status = models.BooleanField(default=True)
+    #models.BooleanField(default=True) apresenta a opção para flegar se esta valida ou não 
 
     def __str__(self):
         return self.title
