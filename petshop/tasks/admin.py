c91a865a37a63db4ebfff79f72f88bff8e6ae295
Felipe Carneiro <felipe_carneiro@outlook.com.br>
2021-12-19 18:23:28 -0300

    atualização

--- petshop/tasks/admin.py
+++ petshop/tasks/admin.py
@@ -1,15 +1,17 @@
 from django.contrib import admin
 from .models import Task
+# para fazer a importação e extração precisa da biblioteca ImportExportModelAdmin
 from import_export.admin import ImportExportModelAdmin
 from tasks.models import Member, Task
 
-
+#banco é novo então precisa registralo
 @admin.register(Member)
 class MemberAdmin(ImportExportModelAdmin):
     list_display = ("firstname", "lastname", "email", "birth_date")
     pass
 
-
+#banco Task já estava cadastrado no app tasks então para que não de mensagem de erro precisa adicionar 
+#como esta abaixo
 @admin.register(Task)
 class TaskAdmin(ImportExportModelAdmin):
     list_display = ("cpf", "phone", "email", "name",
