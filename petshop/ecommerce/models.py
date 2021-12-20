8d2d81694e76d32474166f130858b5c84814d170
Felipe Carneiro <felipe_carneiro@outlook.com.br>
2021-12-19 21:42:54 -0300

    dqw

--- petshop/ecommerce/models.py
+++ petshop/ecommerce/models.py
@@ -67,8 +67,7 @@ class Product(models.Model):
     class Meta:  # verbose_name_plural substitui o nome no banco de dados na pagina do admin
         verbose_name_plural = '2.Produtos'
     
-    class Meta:
-        db_table = 'product'
+
 
     def __str__(self):
         return self.title
